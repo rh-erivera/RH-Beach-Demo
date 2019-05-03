de4f912a0cd550514f38d88ac74fabbe7b2cd27c
Eugene Rivera <erivera@rh-er-21434.restorationhardware.com>
2019-05-03 13:18:26 -0700

    grey logo/ arrow

--- pages/index.vue
+++ pages/index.vue
@@ -6,14 +6,14 @@
         <source src="https://media.restorationhardware.com/is/content/rhis/Waves_v1" type="video/mp4">
       </video>
       <div class="absolute logo">
-        <img src="https://media.restorationhardware.com/is/content/rhis/beach_house/RH-logo-white.svg" alt="RH Beach Logo">
+        <img src="https://media.restorationhardware.com/is/content/rhis/beach_house/RH_Beach_Logo.svg" alt="RH Beach Logo">
       </div>
       <div class="text-center absolute hero-cta w-full">
         <a class="button p-4" href="https://intb-www.restorationhardware.com/content/category.jsp?context=BeachHouseShopAll">SHOP ALL BEACH &#8594;</a>
         <a class="button p-4" href="https://catalogs.restorationhardware.com/RH-Outdoor/1/">SHOP SOURCE BOOK &#8594;</a>
       </div>
       <div id="arrow" class="arrow">
-        <img src="https://media.restorationhardware.com/is/content/rhis/beach_house/scrollDownArrow.svg" alt="bouncing scroll down arrow">
+        <img src="https://media.restorationhardware.com/is/content/rhis/beach_house/scrollDownArrowGrey.svg" alt="bouncing scroll down arrow">
       </div>
     </div>
     <div class="container">
